aff9b81dd2c23374b88515341997483b27d3de67
Toma <tktenshi@mail.ru>
2018-02-04 21:52:02 +0300

    иконки, медиа

--- src/components/Book.js
+++ src/components/Book.js
@@ -29,8 +29,8 @@ class Book extends React.Component {
             <fieldset className="book">
                 <p className="book_text">{this.props.bookName}</p>
                 <p className="book_text">Автор: {this.props.author}</p>
-                <button className="button book_button" onClick={this.btnEditClick}><img className="book_icon" src={require("../imgs/edit3.png")} alt="ред."/></button>
-                <button className="button book_button" onClick={this.btnDelClick}><img className="book_icon" src={require("../imgs/trash.png")} alt="удалить"/></button>
+                <button className="button book_button" onClick={this.btnEditClick}><img className="book_icon--edit" src={require("../imgs/edit.png")} alt="ред."/></button>
+                <button className="button book_button" onClick={this.btnDelClick}><img className="book_icon--del" src={require("../imgs/trash.png")} alt="удалить"/></button>
                 {
                     this.state.typeAlert &&
                     <Alert typeAlert={this.state.typeAlert} alertConfirm={this.alertConfirm}/>
